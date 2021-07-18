9d81ca672b95e932cc054b5c37803d5dead6861c
zampolitx <zampolitx_85@mail.ru>
2021-07-18 22:22:04 +0900

    Javascript if

--- static/js/add_panel.js
+++ static/js/add_panel.js
@@ -2,8 +2,17 @@
 let frm = document.forms.Item_2;
 let building = frm.parent_building;
 let parlor = frm.parent_parlor;
-building.value = 'Корпус1'
+//building.value = 'Корпус1'
 //parlor.value = '1'
+if (building.value == "Корпус1") {
+	parlor.value = "1"
+};
+if (building.value == "Корпус2") {
+	parlor.value = "2"
+};
+if (building.value == "Корпус3") {
+	parlor.value = "3"
+};
 console.dir(building.value);
 let a = 12;
 console.log(parlor.value)
