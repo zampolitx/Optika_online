9e0d3065a052eee0b3b69c17aebbc44fb5153efb
zampolitx <zampolitx_85@mail.ru>
2021-08-04 21:43:29 +0900

    4.08.2021_валидация формы add_building

--- static/js/add_building.js
+++ static/js/add_building.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    alert('sldkf')
+    var form=$(this), btn = form.find('.form_submit');
+    setInterval(function(){
+      //alert('exit setInterval');
+    }, 3000);
+    btn.click(function(){
+      var title = $('.add_building_form').attr('value');
+      console.log(title);
+      if(title=='123'){
+        return false;
+      }
+      else
+        {return false;}//{form.submit();}
+    })
 })
