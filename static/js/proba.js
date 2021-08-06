ed4137e9e0888939cfaaad8e3b6a3b86e1d1b9ae
zampolitx <zampolitx_85@mail.ru>
2021-08-06 16:56:36 +0900

    Callback

--- static/js/proba.js
+++ static/js/proba.js
@@ -1,10 +1,10 @@
-"use strict";
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "http://localhost:5000/proba", true);
-let js = {"my":"second"};
-xhr.send(js);
-xhr.onload = function() {
-	console.log(xhr.status);
-	console.log(xhr.response);
-	console.log(xhr);
-}
+$(document).ready(function(){
+
+	function doHomework(subject, callback) {
+	  alert(`Starting my ${subject} homework.`);
+	  callback();
+	}
+	doHomework('math', function() {
+	  alert('Finished my homework');
+	});
+})
