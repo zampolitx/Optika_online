d3db14ebd6206d82cac9602f192fafab3c394403
zampolitx <zampolitx_85@mail.ru>
2021-08-08 21:16:36 +0900

    proba.js до сих пор не работает

--- static/js/proba.js
+++ static/js/proba.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+	var btn = $('.form_submit');
+	btn.click(function(){
+		console.log('Начало выполнения главной функции');
+		$.when(get_data()).done(function(a1){
+			var res = a1[0];
+			console.log(res);
+		})
+	})
+});
 
-	function doHomework(subject, callback) {
-	  alert(`Starting my ${subject} homework.`);
-	  callback();
-	}
-	doHomework('math', function() {
-	  alert('Finished my homework');
-	});
-})
+
+function get_data() {
+    $.ajax({
+        type: "POST",
+        url: "/get_building",
+        data: $('form').serialize(),
+        type: 'POST',
+        success: function(response) {
+					console.log('Запуск ajax');
+            var json = jQuery.parseJSON(response);  //Прочитать ответ сервера
+            var resp_list = json.resp; //массив с ключем par_buld_resp (из функции get_building main.py)
+            return resp_list;
+        },
+        error: function(error) {
+          console.log(error);
+        }
+    });
+}
