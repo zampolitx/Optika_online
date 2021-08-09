8afdc8fb23411553bdd666011aa4a919ecb7b921
zampolitx <zampolitx_85@mail.ru>
2021-08-09 16:51:40 +0900

    Промисы не работают день 2

--- static/js/proba.js
+++ static/js/proba.js
@@ -1,29 +1,21 @@
-$(document).ready(function(){
-	var btn = $('.form_submit');
-	btn.click(function(){
-		console.log('Начало выполнения главной функции');
-		$.when(get_data()).done(function(a1){
-			var res = a1[0];
-			console.log(res);
-		})
-	})
-});
+function myPromise()
+{
+    return new Promise(function(resolve, reject)
+    {
+        //псевдо асинхронный код
+        var ascync = true; //или  false
+        if (!ascync)
+        return reject(new Error("не удалось выполнить..."));
 
-
-function get_data() {
-    $.ajax({
-        type: "POST",
-        url: "/get_building",
-        data: $('form').serialize(),
-        type: 'POST',
-        success: function(response) {
-					console.log('Запуск ajax');
-            var json = jQuery.parseJSON(response);  //Прочитать ответ сервера
-            var resp_list = json.resp; //массив с ключем par_buld_resp (из функции get_building main.py)
-            return resp_list;
-        },
-        error: function(error) {
-          console.log(error);
-        }
+        return resolve(1);
     });
 }
+
+myPromise()
+.then(function(res)
+{
+    console.log(res); //выведет 1
+})
+.catch(function(err){
+    console.log(err.message); //выведет сообщение "не удалось выполнить..."
+});
