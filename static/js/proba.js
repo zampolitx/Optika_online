0abecf51959b32d93457bcbf1676c591d886b950
zampolitx <zampolitx_85@mail.ru>
2021-08-10 16:54:29 +0900

    Промисы не работают день 2

--- static/js/proba.js
+++ static/js/proba.js
@@ -1,12 +1,48 @@
-async function test() {
-    console.log('1');
-    await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('2');
-            resolve()
-        }, 3000);
+function AJAX(){
+    return new Promise(resolve => {
+        $.ajax({
+            type: "POST",
+            url: "/get_building",
+            data: $("form").serialize(),
+            type: "POST",
+            success: function(response) {
+                var json = jQuery.parseJSON(response);  //Прочитать ответ сервера
+                var resp_list = json.resp; //массив с ключем par_buld_resp (из функции get_building main.py)
+                //console.log(resp_list);
+                resolve(resp_list);
+            },
+            error: function(error) {
+            console.log(error);
+            }
+            
+        })
+        .then(result =>{
+            console.log(result);
+            return result;
+        })
     });
-    console.log('3');
 }
 
-test();
+
+$(document).ready(function(){
+    var btn = $('.form_submit');
+    btn.click(function(){
+        var title = $('.add_building_form').val();
+        async function my() {
+            let result = await AJAX();
+            return result;
+        }
+        let res_my = my();
+        console.log(res_my);
+        if(title=='123'){
+            console.log('no');
+            return false;
+            }
+        else {
+            console.log('yes');
+            return false;            
+        }
+            
+        //{form.submit();}
+    })
+})
