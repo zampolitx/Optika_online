9a99279e4f5fdb94451ac4b1efa4ea47bf0f4d04
zampolitx <zampolitx_85@mail.ru>
2021-08-22 22:10:07 +0900

    Нужно сделать отмену при клике add_building

--- static/js/ex1.js
+++ static/js/ex1.js
@@ -5,7 +5,7 @@ function sendRequest(method, url, body = null) {
         const xhr = new XMLHttpRequest()
         xhr.open(method, url);
         xhr.responseType = 'json';
-        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         xhr.onload = () => {
             if (xhr.status >= 400) {
                 reject(xhr.response)
@@ -16,15 +16,24 @@ function sendRequest(method, url, body = null) {
         xhr.onerror = () => {
             reject(xhr.response)
         }
-        xhr.send(JSON.stringify(body))
+        xhr.send(body)
     })
     
 }
 
-const body = {
-    'building_name=': '123'
-}
-
+/*const body = "building_name=123"
+console.log(typeof body)
+console.log(body)
 sendRequest("POST", requestURL, body)
 .then(data => console.log(data))
-.catch(err => console.log(err))
+.catch(err => console.log(err))*/
+
+let frm = document.forms[0];
+frm.onsubmit = function(event) {
+    if(frm.building_name.value == '123') {
+        event.preventDefault();
+        console.log('123 is')
+    }
+    console.log('click')
+
+}
--- static/js/proba.js
+++ static/js/proba.js
@@ -1,2 +1,41 @@
 'use strict';
-console.log(Array.from(document.body.childNodes).filter);
+
+const requestURL = '/get_building';
+let frm = document.forms[0];
+frm.onsubmit = async function(event) {
+    let body = 'building_name=' + frm.building_name.value;    //получаем 'building_name=123', Если ввели в форму 123
+    let answer = AJAX("POST", requestURL, body)
+    
+    answer.then(data => {
+        if(data.resp=='New'){
+            console.log('Новое здание');
+        }
+        else {
+            return event.preventDefault();
+            console.log('Старое здание');
+            
+        }
+    })
+    .catch(err => console.log(err))
+}
+
+function AJAX(method, url, body = null) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response)
+            } else {
+                resolve(xhr.response)
+            }
+        }
+        xhr.onerror = () => {
+            reject(xhr.response)
+        }
+        xhr.send(body)
+    })
+    
+}
