0f8fb74725364f2cc890e04fab4f5fbcba8cdfe4
zampolitx <zampolitx_85@mail.ru>
2021-08-23 18:57:37 +0900

    Заработала проверка полностью

--- static/js/proba.js
+++ static/js/proba.js
@@ -2,18 +2,18 @@
 
 const requestURL = '/get_building';
 let frm = document.forms[0];
-frm.onsubmit = async function(event) {
+let but = frm.elements[1];
+but.onclick = async function(event) {
     let body = 'building_name=' + frm.building_name.value;    //получаем 'building_name=123', Если ввели в форму 123
-    let answer = AJAX("POST", requestURL, body)
     
+    let answer = AJAX("POST", requestURL, body)
     answer.then(data => {
         if(data.resp=='New'){
             console.log('Новое здание');
+            frm.submit();
         }
         else {
-            return event.preventDefault();
             console.log('Старое здание');
-            
         }
     })
     .catch(err => console.log(err))
